2ce56d8841a4c0321579958a2edab8ba2bee4184
tonisann <haraanton.s@gmail.com>
2023-01-18 00:48:18 +0200

    180120230048

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-'''@app.get("/")
-def read_root():
-    html_content = "<h2>Hello METANIT.COM!</h2>"
-    return HTMLResponse(content=html_content)'''
-
 
 @app.get("/")
-def root():
-    return {"massaje": "Hello Yujin"}
+def func1():
+    mestext1 = {"message": "Hello METANIT.COM"}
+    json_data1 = jsonable_encoder(mestext1)
+    return JSONResponse(content=json_data1, media_type="text/plain")  # {"message": "Hi Yugin23"}
 
 
-@app.get("/about")
-def about():
-    return {"messeje": "how are you"}
+@app.get("/q")
+def finc2():
+    return JSONResponse(content={"message": "hello word"})  # {"message": "hello word"}
+
+
+@app.get("/w")
+def func3():
+    text1 = "hoololo relo olo"
+    return Response(content=text1, media_type="text/plain")
